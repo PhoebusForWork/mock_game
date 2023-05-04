35a440c0fcceea2b12525147aada45690746b2cc
mimiclin <mimiclin.gram@gmail.com>
2023-05-04 14:03:01 +0800

    simplify core logic

--- main.py
+++ main.py
@@ -6,26 +6,13 @@ import json
 app = Flask(__name__)
 
 
-def _get_data(merchant_id, method=''):
+def _get_response_data(merchant_id, method=''):
     if merchant_id == "1":
         merchant_name = "ai"
     else:
         return g.no_specify_merchant
 
-    if method == "login":
-        source = f"data/{merchant_name}/login_response.json"
-    elif method == "order":
-        source = f"data/{merchant_name}/order_response.json"
-    elif method == "transfer":
-        source = f"data/{merchant_name}/transfer_response.json"
-    elif method == "balance":
-        source = f"data/{merchant_name}/balance_response.json"
-    elif method == "deposit":
-        source = f"data/{merchant_name}/deposit_response.json"
-    elif method == "withdraw":
-        source = f"data/{merchant_name}/withdraw_response.json"
-    else:
-        raise Exception("no specify method")
+    source = f"data/{merchant_name}/{method}_response.json"
 
     try:
         with open(source, mode='r', encoding='utf8') as f:
@@ -39,17 +26,19 @@ def _get_data(merchant_id, method=''):
 @app.before_request
 def set_up_data():
     g.key = {"h5AppId": "92d272576dde4a4dc6f9ece81626d641", "appActualKey": "L8IbnjHo8OTu+LBwjQA4dw=="}
+    g.greeting = {"code": 200, "msg": "Hello guest"}
     g.no_specify_merchant = {"code": 400, "msg": "No specify merchant id"}
+    g.no_specify_method = {"code": 400, "msg": "No specify method"}
 
 
-@app.route('/getGameOrderList', methods=['POST'])
-def mock_order():
-    try:
-        param_merchant_id = request.args.get("merchantId", default="1")
-    except Exception:
-        raise
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return jsonify(g.greeting)
 
-    mock_data = _get_data(merchant_id=param_merchant_id, method="order")
+
+@app.route('/getGameOrderList/<merchant_id>', methods=['POST'])
+def mock_order(merchant_id=1):
+    mock_data = _get_response_data(merchant_id=merchant_id, method="order")
     if mock_data["code"] == 200:
         current_timestamp = int(time.time()*1000)
         mock_data["systemTime"] = current_timestamp
@@ -57,14 +46,9 @@ def mock_order():
     return jsonify(mock_data)
 
 
-@app.route('/getTransferOrderList', methods=['POST'])
-def mock_transfer():
-    try:
-        param_merchant_id = request.args.get("merchantId", default="1")
-    except Exception:
-        raise
-
-    mock_data = _get_data(merchant_id=param_merchant_id, method="transfer")
+@app.route('/getTransferOrderList/<merchant_id>', methods=['POST'])
+def mock_transfer(merchant_id=1):
+    mock_data = _get_response_data(merchant_id=merchant_id, method="transfer")
     if mock_data["code"] == 200:
         current_timestamp = int(time.time()*1000)
         mock_data["systemTime"] = current_timestamp
@@ -72,14 +56,9 @@ def mock_transfer():
     return jsonify(mock_data)
 
 
-@app.route('/getBalance', methods=['POST'])
-def mock_balance():
-    try:
-        param_merchant_id = request.args.get("merchantId", default="1")
-    except Exception:
-        raise
-
-    mock_data = _get_data(merchant_id=param_merchant_id, method="balance")
+@app.route('/getBalance/<merchant_id>', methods=['POST'])
+def mock_balance(merchant_id=1):
+    mock_data = _get_response_data(merchant_id=merchant_id, method="balance")
     if mock_data["code"] == 200:
         current_timestamp = int(time.time()*1000)
         mock_data["systemTime"] = current_timestamp
@@ -87,14 +66,9 @@ def mock_balance():
     return jsonify(mock_data)
 
 
-@app.route('/deposit', methods=['POST'])
-def mock_deposit():
-    try:
-        param_merchant_id = request.args.get("merchantId", default="1")
-    except Exception:
-        raise
-
-    mock_data = _get_data(merchant_id=param_merchant_id, method="deposit")
+@app.route('/deposit/<merchant_id>', methods=['POST'])
+def mock_deposit(merchant_id=1):
+    mock_data = _get_response_data(merchant_id=merchant_id, method="deposit")
     if mock_data["code"] == 200:
         current_timestamp = int(time.time()*1000)
         mock_data["systemTime"] = current_timestamp
@@ -102,14 +76,9 @@ def mock_deposit():
     return jsonify(mock_data)
 
 
-@app.route('/withdraw', methods=['POST'])
-def mock_withdraw():
-    try:
-        param_merchant_id = request.args.get("merchantId", default="1")
-    except Exception:
-        raise
-
-    mock_data = _get_data(merchant_id=param_merchant_id, method="withdraw")
+@app.route('/withdraw/<merchant_id>', methods=['POST'])
+def mock_withdraw(merchant_id=1):
+    mock_data = _get_response_data(merchant_id=merchant_id, method="withdraw")
     if mock_data["code"] == 200:
         current_timestamp = int(time.time()*1000)
         mock_data["systemTime"] = current_timestamp
