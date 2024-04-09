287a735c6cffa82e1b317268866c4185c1fa6bb7
tina <sqirrelse@gmail.com>
2024-04-09 16:26:39 +0800

    用戶點數查詢 紅包回調

--- api/mall_callback.py
+++ api/mall_callback.py
@@ -18,4 +18,68 @@ def callback():
     # 返回成功
     return {"status": status_value}, 200
 
+user_balances = {
+    "testUser": {"water": 10, "currentBalance": 20, "P": 100},
+    "user2": {"water": 0, "currentBalance": 0, "P": 0},
+}
 
+#用戶點數查詢
+@mall_callback_bp.route('/qry_current_balance', methods=['POST'])
+def getPoint():
+    # 获取回调内容
+    callback_data = request.json
+
+    # 从回调数据中提取用户名
+    username = callback_data.get('username')
+
+    # 如果用户名不存在于预定义的用户余额字典中，则将其添加到字典中并设置当前余额和水量为100
+    if username not in user_balances:
+        user_balances[username] = {"water": 10, "currentBalance": 11, "P": 12}
+
+    # 获取用户的当前余额和水量
+    current_balance = user_balances[username]["currentBalance"]
+    water_amount = user_balances[username]["water"]
+
+    # 构造用户信息字典
+    user_info = {
+        "username": username,
+        "currentBalance": current_balance,
+        "water": water_amount
+    }
+
+    # 输出回调日志
+    logging.debug('回调参数: %s', callback_data)
+    logging.debug('查詢基本資訊 %s : %s',username ,user_balances[username])
+
+    # 返回成功响应
+    return jsonify({"data": user_info}), 200
+
+#紅包回調
+@mall_callback_bp.route('/gl/task/point/exchange/callback', methods=['POST'])
+def exchange_callback():
+    # 获取回调内容
+    callback_data = request.json
+
+    # 从回调数据中提取用户名、奖励、水量、兑换积分、当前余额
+    username = callback_data.get('username')
+    reward = callback_data.get('reward')
+    water = callback_data.get('water')
+    exchange_points = callback_data.get('exchangePoints')
+    current_balance = callback_data.get('currentBalance')
+
+    # 如果用户名不存在于预定义的用户余额字典中，则返回错误响应
+    if username not in user_balances:
+        return jsonify({"error": "User not found"}), 404
+
+    # 更新用户的当前余额、水量和 P 值
+    user = user_balances[username]
+    logging.debug('原始基本資訊 %s : %s', username, user )
+    user["currentBalance"] += reward  # 奖励
+    user["water"] += water  # 水量
+    user["P"] -= exchange_points  # 兑换积分
+    logging.debug('調整基本資訊 %s : %s', username, user )
+    # 输出回调日志
+    logging.debug('回调参数: %s', callback_data)
+
+    # 返回成功响应
+    return jsonify({"status": "success"}), 200
